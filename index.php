a1011ac05902d5ce86d726688abd544e8cbd0b3d
VISAL TYAGI <107295081+visaltyagi@users.noreply.github.com>
2024-09-07 13:04:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -81,9 +81,9 @@ figure {
                   <figure>
                     <img src="https://raw.githubusercontent.com/Kodeedudey/AWS-DevOps-Project/master/aws.gif" width="300" height="250">
                     <div align="center">
-                    <h2 align="center"> AWS Demo Server Tutorial</h2>
+                    <h2> AWS Demo Server Tutorial</h2>
                     <?php
-                      echo "AWS is the world’s biggest cloud provider which is owned by Amazon. They provide a set of on-demand services to the customers via the internet or 'The Cloud'. Also, you get a pay-as-you-go option where you need to only pay for what you use";
+                        echo "AWS is the world’s biggest cloud provider which is owned by Amazon. They provide a set of on-demand services to the customers via the internet or 'The Cloud'. Also, you get a pay-as-you-go option where you need to only pay for what you use";
                     ?>
                     </div>
                   </figure>
